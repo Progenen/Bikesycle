fa6116364e0a7dd679d7c994fbc9d85958187a25
Stanislav <stanislav_2010@icloud.com>
2020-07-27 14:19:47 +0500

    start

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+function ibg(){
+
+    let ibg=document.querySelectorAll(".ibg");
+    for (var i = 0; i < ibg.length; i++) {
+    if(ibg[i].querySelector('img')){
+    ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+    }
+    }
+    }
+    
+    ibg();
+    
+$('.burger-menu').click(function(event) {
+    $('.burger-menu, .menu__body').toggleClass('active');
+    $('body').toggleClass('lock');
+});
